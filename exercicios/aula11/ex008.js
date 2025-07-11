429b19c75e03bbd7c7b169da91abbd779fe36bf7
Gabriel Matos <gabrielmatosf2005@gmail.com>
2025-07-11 05:25:46 -0300

    .

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro e de ${vel}Km/h`)
+if(vel > 61){ //condição simples
+    console.log(`você ultrapassou o limite de velocidade MULTADO`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
