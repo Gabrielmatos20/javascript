b241d289f4dfa2f544dd646a8eebccdb4cad5ac1
Gabriel Matos <gabrielmatosf2005@gmail.com>
2025-07-20 00:48:17 -0300

    .

---
+++ exercicios/aula013/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do{
+    console.log('tudo bem?')
+    c++
+}while (c < 7)
+
+
+
+/*
+
+var c = 1
+while (c < 7){
+    console.log('tudo bem?')
+    c++
+}
+
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
