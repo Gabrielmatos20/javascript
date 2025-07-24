3f9605cbfcfc3348b64df0d73585cd863dd31cd5
Gabriel Matos <gabrielmatosf2005@gmail.com>
2025-07-24 02:46:29 -0300

    .

---
+++ exercicios/aula14ex/ex017/scripts.js
@@ -0,0 +1,17 @@
+function gerarTabuada(){
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um numero!')
+    }else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1; c <=10; c++ ){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+
+    }
+}
